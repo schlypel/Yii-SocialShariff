04d06b98cdc68ba2077e6d56423ba61caa8fedcf
schlypel <schlypel@users.noreply.github.com>
2015-06-25 17:29:08 +0200

    added lang parameter to be set from yiis config

--- views/socialShariff.php
+++ views/socialShariff.php
@@ -1,1 +1,1 @@
-<div class="shariff" data-orientation="<?php echo $orientation; ?>" data-theme="<?php echo $theme; ?>"></div>
+<div class="shariff" data-orientation="<?php echo $orientation; ?>" data-theme="<?php echo $theme; ?>" data-lang="<?php echo Yii::app()->language; ?>"></div>
